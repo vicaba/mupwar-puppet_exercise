ccb4d381dde4a1279eff0bef852c4db98adba198
vicaba <vicaba@me.com>
2015-11-13 23:21:30 +0100

    PHP added.

--- manifests/init.pp
+++ manifests/init.pp
@@ -73,6 +73,30 @@ class deploying {
 		}
 	}
 
+	$php_version = '56'
+
+	include ::yum::repo::remi
+
+	if $php_version == '55' {
+	    include ::yum::repo::remi_php55
+	}
+	elsif $php_version == '56'{
+	    ::yum::managed_yumrepo { 'remi-php56':
+	      descr          => 'Les RPM de remi pour Enterpise Linux $releasever - $basearch - PHP 5.6',
+	      mirrorlist     => 'http://rpms.famillecollet.com/enterprise/$releasever/php56/mirror',
+	      enabled        => 1,
+	      gpgcheck       => 1,
+	      gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
+	      gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-remi',
+	      priority       => 1,
+	    }
+	}
+
+	class { 'php': 
+	    version => 'latest',
+	    require => Yumrepo['remi-php56']
+	}
+
 	include development::create_files
 
 	include epel
