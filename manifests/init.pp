1f7939e102571573672c071e00c5ae67a2de7090
vicaba <vicaba@me.com>
2015-11-13 19:20:29 +0100

    First Commit. The module isn't ready yet.

---
+++ manifests/init.pp
@@ -0,0 +1,80 @@
+class deploying {
+
+	file {'/var/www/':
+		ensure => 'directory',
+		path => '/var/www/',
+		mode => '1777',
+		owner => 'root',
+		group => 'root',
+		before => Class['apache']
+	}
+
+	file {'/var/www/project1/':
+		ensure => 'directory',
+		path => '/var/www/project1/',
+		mode => '1777',
+		owner => 'root',
+		group => 'root',
+		before => Class['apache']
+	}
+
+
+	class {'apache':
+	}
+
+	# Las dependencias entre apache y los vhosts se gestionan internamente
+	apache::vhost { 'centos.dev':
+		port    => '8080',
+		docroot => '/var/www/',
+	}
+
+	apache::vhost { 'project1.dev':
+		port    => '8080',
+		docroot => '/var/www/project1/',
+	}
+
+	class { '::mysql::server':
+		root_password => 'vagrantpass',
+	}
+
+	mysql::db { 'mpwar_test':
+		user => 'vagrant',
+	 	password => 'mpwardb',
+	}
+
+
+	host { 'mysql1':
+		ip => '127.0.0.1',
+	}
+
+	host { 'memcached1':
+		ip => '127.0.0.1',
+	}
+
+	class development::create_files {
+		file {'/var/www/index.php':
+			ensure => 'file',
+			path => '/var/www/index.php',
+			mode => '1777',
+			owner => 'root',
+			group => 'root',
+			require => File['/var/www/']
+		}
+
+		file {'/var/www/project1/index.php':
+			ensure => 'file',
+			path => '/var/www/project1/index.php',
+			mode => '1777',
+			owner => 'root',
+			group => 'root',
+			require => File['/var/www/project1/']
+		}
+	}
+
+	include development::create_files
+
+	include epel
+
+}
+
+
